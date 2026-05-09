a8592be77bfae3720bd72ac4a216c166099474d4
Mark Goulding <markgoulding180@gmail.com>
2026-05-09 08:11:32 +0100

    Fixed double Layout wrapper - each page handles its own layout

--- projects/CRM-CONSULTANCY/src/App.jsx
+++ projects/CRM-CONSULTANCY/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard';
 import Contacts from './pages/Contacts';
 import Deals from './pages/Deals';
@@ -11,17 +10,15 @@ import Invoices from './pages/Invoices';
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/deals" element={<Deals />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/activity" element={<Activity />} />
-          <Route path="/invoices" element={<Invoices />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/invoices" element={<Invoices />} />
+      </Routes>
     </BrowserRouter>
   );
 }
